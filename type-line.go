e7c5d0ce3a03cd6f8ebe00d47cb481056c4873df
Tom Mornini <tom@subledger.com>
2017-05-17 22:49:28 -0700

    improve variable name: ok -> known

--- type-line.go
+++ type-line.go
@@ -132,9 +132,9 @@ func (line *line) substitute(context *context) error {
 	substitutedLine := parts[0]
 
 	for i := 1; i < count-1; i += 2 {
-		substitution, ok := context.Substitutions[parts[i]]
+		substitution, known := context.Substitutions[parts[i]]
 
-		if !ok {
+		if !known {
 			return fmt.Errorf("unknown tag: %v", parts[i])
 		}
 
